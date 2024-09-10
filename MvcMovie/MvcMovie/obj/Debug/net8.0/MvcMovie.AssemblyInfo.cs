915e85fda891b9cc617102abeb865157ddbf0a2b
arisha1-bit <arishastark5@gmail.com>
2024-09-10 22:39:04 +0300

    I`ve created view for my web app

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cbae35f6c050b90925052bcc1fb4735a8f0f20a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc50a6b13f0a66a245931bab35a185a869fd976d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
